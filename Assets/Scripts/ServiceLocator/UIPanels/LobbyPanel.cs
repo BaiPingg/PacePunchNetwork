22f30f2ba3b660d5ebada79a5800a3f393c7f296
BaipPing <15281485550@163.com>
2024-05-28 22:26:42 +0800

    state

--- Assets/Scripts/ServiceLocator/UIPanels/LobbyPanel.cs
+++ Assets/Scripts/ServiceLocator/UIPanels/LobbyPanel.cs
@@ -50,7 +50,6 @@ public class LobbyPanel : UIPanel
 
         if (allReady)
         {
-            
         }
     }
 
@@ -89,6 +88,11 @@ public class LobbyPanel : UIPanel
 
     public void OnAddPlayerToPlayerLists(PlayerBaseInfo playerInfo)
     {
+        if (_PlayerCards.ContainsKey(playerInfo.clientId))
+        {
+            return;
+        }
+
         var card = Instantiate(_playerCardTemplate.gameObject, _playerListContent.transform)
             .GetComponent<PlayerCard>();
         card.Init(playerInfo);
